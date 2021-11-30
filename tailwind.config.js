ecbcb9b8522ced2682ae163d71f0bd008f8bdd3b
Rakib Hasan <rakib.cs132@gmail.com>
2021-11-30 09:50:26 +0600

    landing page done

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily : {
+        lora: "'Lora', serif",
+        roboto: "'Roboto', sans-serif"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
